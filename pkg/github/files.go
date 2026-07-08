d8f95d9b9bfb44fd61a57261b5b80a17b52d59bd
Bart Smykla <bartek@smykla.com>
2026-07-08 15:31:29 +0200

    fix(sync): honor default merge strategy (#170)
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/github/files.go
+++ pkg/github/files.go
@@ -276,10 +276,12 @@ func processFileMapping(
 	var mergeStrategy configtypes.MergeStrategy
 
 	if mergeConfig != nil {
+		effectiveStrategy := effectiveMergeStrategy(mapping.Dest, mergeConfig)
+
 		log.Info(
 			"found merge config for file",
 			"file", mapping.Dest,
-			"strategy", mergeConfig.Strategy,
+			"strategy", effectiveStrategy,
 		)
 
 		// Apply merge
@@ -292,7 +294,7 @@ func processFileMapping(
 		} else {
 			// Use merged content
 			sourceContent = mergedContent
-			mergeStrategy = mergeConfig.Strategy
+			mergeStrategy = effectiveStrategy
 		}
 	}
 
@@ -384,6 +386,23 @@ func processNewFile(
 		Content: sourceContent,
 		Action:  fileActionCreate,
 	})
+}
+
+func effectiveMergeStrategy(path string, mergeConfig *configtypes.FileMergeConfig) configtypes.MergeStrategy {
+	if mergeConfig == nil {
+		return ""
+	}
+
+	if mergeConfig.Strategy != "" {
+		return mergeConfig.Strategy
+	}
+
+	ext := strings.ToLower(filepath.Ext(path))
+	if ext == ".md" || ext == ".markdown" {
+		return configtypes.MergeStrategyMarkdown
+	}
+
+	return configtypes.MergeStrategyDeep
 }
 
 // shouldSkipRenovateJSON checks if renovate.json should be skipped due to manual modifications.
